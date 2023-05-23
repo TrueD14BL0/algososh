a8021a02aa27881b5bed80516154e44ab36922ef
Mikhail <d14bl0@mail.ru>
2023-05-23 22:36:30 +0300

    sorting try 1

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -4,19 +4,30 @@ import styles from './sorting-page.module.css';
 import { Column } from "../ui/column/column";
 import { RadioInput } from "../ui/radio-input/radio-input";
 import { Button } from "../ui/button/button";
+import { DELAY_IN_MS } from "../../constants/delays";
 import { Direction } from "../../types/direction";
+import { ElementStates } from "../../types/element-states";
 
 export const SortingPage: React.FC = () => {
 
-  const [renderArray, setRenderArray] = useState<number[]>([]);
+  type TArrElement ={
+    value: number,
+    type: ElementStates
+  }
+
+  const [renderArray, setRenderArray] = useState<TArrElement[]>([]);
   const [isStart, setStart] = useState(false);
   const [sortType, setSortType] = useState('Выбор');
-
-  const generateNewArr = (): number[] => {
+//(Math.floor(Math.random() * 100)
+  const randomArr = (): TArrElement[] => {
     const qtyEl = Math.floor(Math.random() * 14+3);
-    const arrToRender: number[] = [];
+    const arrToRender: TArrElement[] = [];
     for (let index = 0; index < qtyEl; index++) {
-      arrToRender.push(Math.floor(Math.random() * 100));
+      const val = Math.floor(Math.random() * 100);
+      arrToRender.push({
+        value: val, 
+        type: ElementStates.Default,
+      });
     }
     return arrToRender;
   }
@@ -27,15 +38,47 @@ export const SortingPage: React.FC = () => {
 
   const renderNewArr = () => {
     setStart(true);
-    setRenderArray(generateNewArr);
+    setRenderArray(randomArr);
     setStart(false);
   }
+
+  const sortByChoise = (asc: boolean) => {
+    for (let i = 0; i < renderArray.length; i++) {
+      let arrForWork = renderArray.slice(0);
+      let elToCompare = arrForWork[i];
+      elToCompare.type = ElementStates.Changing;
+      setRenderArray(arrForWork);
+      for (let j = i+1; j < arrForWork.length; j++) {
+        let arrForWork = renderArray.slice(0);
+        let ecurrentEl = arrForWork[j];
+        ecurrentEl.type = ElementStates.Changing;
+        setRenderArray(arrForWork);
+        if (elToCompare.value > arrForWork[j].value){
+          elToCompare = arrForWork[j];
+        }
+        arrForWork = renderArray.slice(0);
+        ecurrentEl = arrForWork[j];
+        ecurrentEl.type = ElementStates.Default;
+        setRenderArray(arrForWork); 
+      }
+      arrForWork = arrForWork.slice(0);
+      elToCompare.type = ElementStates.Modified;
+    }
+
+  }
+
   const sortByAsc = () => {
-    setStart(true);
+    //setStart(true);
+    if(sortType==='Выбор'){
+      sortByChoise(true);
+    }
   }
 
   const sortByDesc = () => {
-    setStart(true);
+    //setStart(true);
+    if(sortType==='Выбор'){
+      sortByChoise(false);
+    }
   }
 
   return (
@@ -55,7 +98,7 @@ export const SortingPage: React.FC = () => {
       </div>
       <div className={styles.flex}>
         {
-          renderArray.map((element, index)=><Column index={element} key={index}/>)
+          renderArray.map((element, index)=><Column index={element.value} key={index} state={element.type} />)
         }
       </div>
     </SolutionLayout>
