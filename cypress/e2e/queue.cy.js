1dd2413aee52c34e760b649275c7c4875dadb431
Mikhail <d14bl0@mail.ru>
2023-06-28 21:46:52 +0300

    queue e2e test done

---
+++ cypress/e2e/queue.cy.js
@@ -0,0 +1,90 @@
+import { SHORT_DELAY_IN_MS } from "../../src/constants/delays";
+import { BORDER_CHANGING, BORDER_DEFAULT, CIRCLE_BORDER_SELECTOR, CIRCLE_ELEMENT_SELECTOR, CIRCLE_HEAD_SELECTOR, CIRCLE_LETTER_SELECTOR, CIRCLE_TAIL_SELECTOR, LOADER_SELECTOR } from "../constants/cy-constants";
+
+describe('app works correctly with routes', function() {
+  beforeEach(function() {
+    cy.visit('/queue');
+  });
+
+  it('should be queue page', function() {
+    cy.contains('Очередь');
+  });
+
+  it('should button disabled while input empty', function() {
+    cy.get('button').eq(1).should('be.disabled');
+    cy.get('input').should('be.empty');
+  });
+  
+  it('should add element', function() {
+    for (let i = 0; i < 5; i++) {
+      cy.get('input').type(i);
+      cy.get('button').eq(1).should('be.not.disabled').as('add_button');
+      cy.get('@add_button').click();
+      cy.get('@add_button').get(LOADER_SELECTOR).should('exist');
+      cy.get(CIRCLE_ELEMENT_SELECTOR).each((circle, index, collection)=>{
+        if(index===i){
+          cy.wrap(circle).find(CIRCLE_BORDER_SELECTOR).should('have.css', 'border-color', BORDER_CHANGING);
+        }else{
+          cy.wrap(circle).find(CIRCLE_BORDER_SELECTOR).should('have.css', 'border-color', BORDER_DEFAULT);
+          if(index<i){
+            cy.wrap(circle).find(CIRCLE_BORDER_SELECTOR).should('have.text', index);
+          }
+        }
+      });
+      cy.wait(SHORT_DELAY_IN_MS);
+      cy.get(CIRCLE_ELEMENT_SELECTOR).first().find(CIRCLE_HEAD_SELECTOR).should('exist').and('have.text', 'head');
+      cy.get(CIRCLE_ELEMENT_SELECTOR).eq(i).find(CIRCLE_BORDER_SELECTOR).should('have.css', 'border-color', BORDER_DEFAULT);
+      cy.get(CIRCLE_ELEMENT_SELECTOR).eq(i).find(CIRCLE_TAIL_SELECTOR).should('exist').and('have.text', 'tail');
+    }
+  });
+
+  it('should del element', function() {
+
+    const lastIndexAddingElements = 4;
+
+    for (let i = 0; i <= lastIndexAddingElements; i++) {
+      cy.get('input').type(i);
+      cy.get('button').eq(1).should('be.not.disabled').as('add_button');
+      cy.get('@add_button').click();
+      cy.wait(SHORT_DELAY_IN_MS);
+    }
+
+    for (let i = 0; i < 3; i++) {
+      cy.get('button').eq(2).should('be.not.disabled').as('del_button');
+      cy.get('@del_button').click();
+      cy.get('@del_button').get(LOADER_SELECTOR).should('exist');
+      cy.get(CIRCLE_ELEMENT_SELECTOR).each((circle, index, collection)=>{
+        if(index===i){
+          cy.wrap(circle).find(CIRCLE_HEAD_SELECTOR).and('have.text', 'head');
+          cy.wrap(circle).find(CIRCLE_BORDER_SELECTOR).should('have.css', 'border-color', BORDER_CHANGING);
+        }else{
+          cy.wrap(circle).find(CIRCLE_BORDER_SELECTOR).should('have.css', 'border-color', BORDER_DEFAULT);
+        }
+      });
+      cy.wait(SHORT_DELAY_IN_MS);
+      cy.get(CIRCLE_ELEMENT_SELECTOR).eq(i).find(CIRCLE_BORDER_SELECTOR).should('have.css', 'border-color', BORDER_DEFAULT);
+      cy.get(CIRCLE_ELEMENT_SELECTOR).eq(i).find(CIRCLE_HEAD_SELECTOR).should('be.empty');
+      cy.get(CIRCLE_ELEMENT_SELECTOR).eq(i+1).find(CIRCLE_HEAD_SELECTOR).should('have.text', 'head');
+    }
+
+  });
+
+  it('should clear queue', function() {
+    const lastIndexAddingElements = 4;
+    for (let i = 0; i <= lastIndexAddingElements; i++) {
+      cy.get('input').type(i);
+      cy.get('button').eq(1).should('be.not.disabled').as('add_button');
+      cy.get('@add_button').click();
+      cy.wait(SHORT_DELAY_IN_MS);
+    }
+
+    cy.get('button').eq(3).should('be.not.disabled').as('clear_button');
+    cy.get('@clear_button').click();
+    cy.get(CIRCLE_ELEMENT_SELECTOR).each((circle)=>{
+      cy.wrap(circle).find(CIRCLE_LETTER_SELECTOR).should('be.empty');
+      cy.wrap(circle).find(CIRCLE_HEAD_SELECTOR).should('be.empty');
+      cy.wrap(circle).find(CIRCLE_TAIL_SELECTOR).should('be.empty');
+    });
+  });
+
+});
--- cypress/e2e/stack.cy.js
+++ cypress/e2e/stack.cy.js
@@ -1,5 +1,5 @@
 import { SHORT_DELAY_IN_MS } from "../../src/constants/delays";
-import { BORDER_CHANGING, BORDER_DEFAULT, CIRCLE_BORDER_SELECTOR, CIRCLE_ELEMENT_SELECTOR, CIRCLE_HEAD_SELECTOR, CIRCLE_INDEX_SELECTOR, CIRCLE_LETTER_SELECTOR } from "../constants/cy-constants";
+import { BORDER_CHANGING, BORDER_DEFAULT, CIRCLE_BORDER_SELECTOR, CIRCLE_ELEMENT_SELECTOR, CIRCLE_HEAD_SELECTOR, CIRCLE_INDEX_SELECTOR, CIRCLE_LETTER_SELECTOR, LOADER_SELECTOR } from "../constants/cy-constants";
 
 describe('app works correctly with routes', function() {
   beforeEach(function() {
@@ -20,6 +20,7 @@ describe('app works correctly with routes', function() {
       cy.get('input').type(i);
       cy.get('button').eq(1).should('be.not.disabled').as('add_button');
       cy.get('@add_button').click();
+      cy.get("@add_button").get(LOADER_SELECTOR).should('exist');
       cy.get(CIRCLE_ELEMENT_SELECTOR).should('have.length', i+1);
       cy.get(CIRCLE_ELEMENT_SELECTOR).each((circle, index, collection)=>{
         if(index<collection.length-1){
@@ -27,7 +28,7 @@ describe('app works correctly with routes', function() {
           cy.wrap(circle).find(CIRCLE_HEAD_SELECTOR).should('be.empty');
         }else{
           cy.wrap(circle).find(CIRCLE_BORDER_SELECTOR).should('have.css', 'border-color', BORDER_CHANGING);
-          cy.wrap(circle).find(CIRCLE_HEAD_SELECTOR).should('exist').and('have.text', 'top');
+          cy.wrap(circle).find(CIRCLE_HEAD_SELECTOR).and('have.text', 'top');
         }
         cy.wrap(circle).find(CIRCLE_LETTER_SELECTOR).should('have.text', index);
         cy.wrap(circle).find(CIRCLE_INDEX_SELECTOR).should('have.text', index);
@@ -35,6 +36,55 @@ describe('app works correctly with routes', function() {
       cy.wait(SHORT_DELAY_IN_MS);
     }
     cy.get(CIRCLE_ELEMENT_SELECTOR).last().find(CIRCLE_BORDER_SELECTOR).should('have.css', 'border-color', BORDER_DEFAULT);
+    cy.get("@add_button").get(LOADER_SELECTOR).should('not.exist');
   });
 
+  it('should del element', function() {
+    for (let i = 0; i < 5; i++) {
+      cy.get('input').type(i);
+      cy.get('button').eq(1).should('be.not.disabled').as('add_button');
+      cy.get('@add_button').click();
+      cy.wait(SHORT_DELAY_IN_MS);
+    }
+    for (let i = 0; i < 2; i++) {
+      cy.get('button').eq(2).should('be.not.disabled').as('del_button');
+      cy.get('@del_button').click();
+      cy.get("@del_button").get(LOADER_SELECTOR).should('exist');
+      cy.get(CIRCLE_ELEMENT_SELECTOR).each((circle, index, collection)=>{
+        if(index<collection.length-1){
+          cy.wrap(circle).find(CIRCLE_BORDER_SELECTOR).should('have.css', 'border-color', BORDER_DEFAULT);
+          cy.wrap(circle).find(CIRCLE_HEAD_SELECTOR).should('be.empty');
+        }else{
+          cy.wrap(circle).find(CIRCLE_BORDER_SELECTOR).should('have.css', 'border-color', BORDER_CHANGING);
+          cy.wrap(circle).find(CIRCLE_HEAD_SELECTOR).and('have.text', 'top');
+        }
+        cy.wrap(circle).find(CIRCLE_LETTER_SELECTOR).should('have.text', index);
+        cy.wrap(circle).find(CIRCLE_INDEX_SELECTOR).should('have.text', index);
+      });
+      cy.wait(SHORT_DELAY_IN_MS);
+      cy.get(CIRCLE_ELEMENT_SELECTOR).each((circle, index, collection)=>{
+        if(index<collection.length-1){
+          cy.wrap(circle).find(CIRCLE_HEAD_SELECTOR).should('be.empty');
+        }else{
+          cy.wrap(circle).find(CIRCLE_HEAD_SELECTOR).and('have.text', 'top');
+        }
+        cy.wrap(circle).find(CIRCLE_BORDER_SELECTOR).should('have.css', 'border-color', BORDER_DEFAULT);
+        cy.wrap(circle).find(CIRCLE_LETTER_SELECTOR).should('have.text', index);
+        cy.wrap(circle).find(CIRCLE_INDEX_SELECTOR).should('have.text', index);
+      });
+      cy.get("@del_button").get(LOADER_SELECTOR).should('not.exist');
+    }
+  });
+
+  it('should clear stack', function() {
+    for (let i = 0; i < 5; i++) {
+      cy.get('input').type(i);
+      cy.get('button').eq(1).should('be.not.disabled').as('add_button');
+      cy.get('@add_button').click();
+      cy.wait(SHORT_DELAY_IN_MS);
+    }
+    cy.get('button').eq(3).should('be.not.disabled').as('clear_button');
+    cy.get('@clear_button').click();
+    cy.get(CIRCLE_ELEMENT_SELECTOR).should('have.length', 0);
+  });
 });
